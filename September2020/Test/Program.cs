2e4e5dcabd0e3a3c4ac0b42bbaf8a20cd511aab6
kumaravel.kartikeyan@gmail.com <kumaravel.kartikeyan@gmail.com>
2020-09-21 20:53:31 +1200

    implemented NUnit

--- September2020/Test/Program.cs
+++ September2020/Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using September2020.Pages;
 using System;
@@ -6,18 +7,26 @@ using System.Threading;
 
 namespace September2020
 {
-
+    [TestFixture]
     class Program
     {
-        static void Main(string[] args)
+        // init driver
+        IWebDriver driver;
+
+        [SetUp]
+        public void LoginToTurnUp()
         {
-            // Init and define webdriver
-            IWebDriver driver = new ChromeDriver();
+            // define webdriver
+            driver = new ChromeDriver();
 
             // Object init and define for login page
             LoginPage loginObj = new LoginPage();
             loginObj.LoginSteps(driver);
+        }
 
+        [Test]
+        public void CreateNewTMTest()
+        {
             // Object init and define for home page
             HomePage homeObj = new HomePage();
             homeObj.NavigateToTM(driver);
@@ -27,16 +36,42 @@ namespace September2020
             // Object init and define for TM page
             TMPage tmObj = new TMPage();
             tmObj.CreateTM(driver);
-
-            // Test 2 - Check if the user is able to edit time successfully
-            tmObj.EditTM(driver);
-
-            // Test 2 - Check if the user is able to delete time successfully
-            tmObj.DeleteTM(driver);
-
-
         }
 
+        [Test]
+        public void EditTMTest()
+        {
+            // Object init and define for home page
+            HomePage homeObj = new HomePage();
+            homeObj.NavigateToTM(driver);
+
+            // Test 2 - Check if the user is able to edit time successfully
+
+            // Object init and define for TM page
+            TMPage tmObj = new TMPage();
+            tmObj.EditTM(driver);
+        }
+
+        [Test]
+        public void DeleteTMTest()
+        {
+            // Object init and define for home page
+            HomePage homeObj = new HomePage();
+            homeObj.NavigateToTM(driver);
+
+            // Test 3 - Check if the user is able to delete time successfully
+
+            // Object init and define for TM page
+            TMPage tmObj = new TMPage();
+            tmObj.DeleteTM(driver);
+        }
+
+        [TearDown]
+        public void TestClosure()
+        {
+            // close instances of open chrome driver 
+            driver.Quit();
+        }
 
     }
 
